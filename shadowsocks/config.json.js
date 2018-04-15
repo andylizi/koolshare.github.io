23607d4e82efdffe9eec2511f5f82ed5ea1f235f
sadoneli <sadoneli@gmail.com>
2018-04-15 22:02:07 +0800

    update binary for shadowsocks

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.8.7",
-"md5":"9d07826dfe622c1d1c57e728b5a59071",
+"version":"3.8.8",
+"md5":"8155207d5b45347d5e03eb02d406afb9",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-04-15_19:11:14"
+"build_date":"2018-04-15_22:01:50"
 }
