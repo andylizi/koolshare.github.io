c2611a8e307f29b51ba3ce0529f9f47058f0b101
sadoneli <sadoneli@gmail.com>
2018-04-16 12:36:22 +0800

    update shadowsocks to 3.7.9

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.8.8",
-"md5":"8155207d5b45347d5e03eb02d406afb9",
+"version":"3.8.9",
+"md5":"e0f083ac7ef5c57993c148f881f32fa4",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-04-15_22:01:50"
+"build_date":"2018-04-16_12:36:07"
 }
